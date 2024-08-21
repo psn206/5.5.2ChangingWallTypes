c19e3f9099296374be4c1d15aa8ebd68c587390e
Sergei Prutsakov <psn206@mail.ru>
2024-08-21 15:27:14 +0300

    Добавьте файлы проекта.

---
+++ ChangingWallTypes/MainViewViewModel.cs
@@ -0,0 +1,75 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ChangingWallTypes
+{
+    internal class MainViewViewModel
+    {
+
+        private ExternalCommandData _commandData;
+
+        public DelegateCommand SaveCommand { get; }
+
+        public List<Element> PickedWalls { get; }
+
+        public List<WallType> WallsSystem { get; } = new List<WallType>();
+
+        public WallType SelectedWallsSystem { get; set; }
+        MethodRevitApp methodRevitApp;
+
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+            methodRevitApp = new MethodRevitApp(commandData);
+            PickedWalls = methodRevitApp.GetWalls();
+            WallsSystem = methodRevitApp.GetSystemType();
+
+        }
+
+
+        private void OnSaveCommand()
+        {
+            UIApplication uiApp = _commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            if (WallsSystem.Count == 0 || SelectedWallsSystem == null) { return; }
+
+            using (var ts = new Transaction(doc, "Set Type"))
+            {
+                ts.Start();
+                foreach (var PickedWall in PickedWalls)
+                {
+                    if (PickedWall is Wall)
+                    {
+                        var oWall = PickedWall as Wall;
+                        oWall.WallType = SelectedWallsSystem;
+                    }
+                }
+                ts.Commit();
+
+                RaiseCloseRecuest();
+            }
+
+          
+        }
+
+        public event EventHandler CloseRecuest;
+
+        public void RaiseCloseRecuest()
+        {
+            CloseRecuest?.Invoke(this, EventArgs.Empty);
+        }
+
+    }
+}
---
+++ ChangingWallTypes/MethodRevitApp.cs
@@ -0,0 +1,45 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangingWallTypes
+{
+    internal class MethodRevitApp
+    {
+        UIApplication uiApp;
+        UIDocument uiDoc;
+        Document doc;
+
+        public UIApplication UiApp { get => uiApp;  }
+        public UIDocument UiDoc { get => uiDoc; }
+        public Document Doc { get => doc; }
+
+        public MethodRevitApp(ExternalCommandData commandData)
+        {
+           uiApp = commandData.Application;
+            uiDoc = UiApp.ActiveUIDocument;
+            doc = UiDoc.Document;
+        }
+
+        public List<Element> GetWalls()
+        {
+            var selectElements = UiDoc.Selection.PickObjects(ObjectType.Element, "Выберете стены");
+            List<Element> walls = selectElements.Select(selectElement => Doc.GetElement(selectElement)).ToList();
+            return walls;
+        }
+
+        public List<WallType> GetSystemType()
+        {
+            List<WallType> wallTypes = new FilteredElementCollector(Doc)
+               .OfClass(typeof(WallType))
+               .Cast<WallType>()
+               .ToList();
+            return wallTypes;
+        }
+    }
+}
